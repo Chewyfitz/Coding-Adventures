9f87b1772dc047160ea3e840051ab5ad65447e23
Chewyfitz <Chewyfitz@gmail.com>
2021-08-14 16:32:14 +1000

    Added a simple loop program to OpenMP/simple

---
+++ OpenMP/simple/readme.md
@@ -0,0 +1,2 @@
+# Simple programs
+These are the most basic programs imaginable, taking simple inputs and performing simple computations which don't reasonably require parallelisation
---
+++ OpenMP/simple/simple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void main() {
+  #pragma omp parallel for schedule(dynamic)
+	for(int i=0; i<15; i++) {
+		printf("[%d] Print from thread %d\n", i, i);
+	}
+}
