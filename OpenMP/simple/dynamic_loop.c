adc40733d22e880566d1ce549ab63d15290f5d12
Chewyfitz <Chewyfitz@gmail.com>
2021-08-14 17:04:43 +1000

    Renamed plain_loop.c, added input args

--- OpenMP/simple/dynamic_loop.c
+++ OpenMP/simple/dynamic_loop.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
+int main(int argc, char** argv) {
+  int num_loops = 15;
+  if(argc == 2) {
+    // An argument was supplied, use that as the number of loops
+    num_loops = atoi(argv[1]);
+  } else if (argc > 2) {
+    printf("Usage: ./%s [num_loops]\n", argv[0]);
+    return 1;
+  }
+  
 	#pragma omp parallel for
-	for(int i=0; i<15; i++) {
+	for(int i=0; i<num_loops; i++) {
 		printf("[%d] Print from thread %d\n", i, i);
 	}
+  return 0;
 }
--- OpenMP/simple/plain_loop.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main() {
-	#pragma omp parallel for
-	for(int i=0; i<15; i++) {
-		printf("[%d] Print from thread %d\n", i, i);
-	}
-}
